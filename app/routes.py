7ee504e3708765075c9e5516847e29827dce2bce
Caroline-Molina <carolinebstmolina@gmail.com>
2024-11-10 13:16:10 -0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app import app
+from app import _init_
 
 from flask import render_template
 
@@ -12,18 +13,41 @@ def index():
 
 @app.route('/produto')
 def produto():
+    conectar = conectar_db()
+    cursor = conectar.cursor()
+    
+    cursor.execute(
+            'SELECT * FROM produto'
+    )
+    #produto = cursor.fetchall()
+    conectar.close()
 
     return render_template('produto.html')
 
 
 
-@app.route('/adicionar')
+@app.route('/adicionar', methods = ['POST'])
 def adicionar():
+    if request.method == 'POST':
+        nome = request.form['Nome']
+        preco = request.form['Preço']
+        quant = request.form['Quantidade']
+        val = request.form['Validade']
+        desc = request.form['Descrição']
 
-    return render_template('adicionar.html')
+        conectar = conectar_db()
+        cursor = conectar.cursor()
+        
+        cursor.execute(
+            'INSERT INTO produto (nomeProduto, preco, quantidade, validade, descricao) VALUES (?, ?, ?, ?, ?)', (nome, preco, quant, val, desc)
+        )
+        
+        conectar.commit()
+        conectar.close()
+        flash('Produto adicionado com sucesso!', 'success')
 
-
-
+        return render_template('adicionar.html')
 
 if __name__ == '__main__':
+    criar_tabela_produto()
     app.run(debug=True)
