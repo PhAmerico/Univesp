5a340ae86bd847372a4c424faf93343bb4ab2c06
Caroline-Molina <carolinebstmolina@gmail.com>
2024-11-10 12:55:18 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,28 @@
-from flask import Flask
+from flask import Flask, flash
+from app import routes
+
+import sqlite3
 
 app = Flask(__name__)
 
-from app import routes
+#conexão do BD ao back-end
+def conectar_db():
+  conectar = sqlite3.connect('banco.db')
+  return conectar
+
+def criar_tabela_produto():
+  conectar = conectar_db()
+  cursor = conectar.cursor()
+  cursor.execute ('''
+    create table if not exists produto (
+      idProduto INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      nomeProduto TEXT NOT NULL,
+      preco NUMERIC NOT NULL, 
+      quantidade NUMERIC NOT NULL,
+      validade TEXT NOT NULL,
+      descricao TEXT NOT NULL,
+    )
+  ''')
+  conectar.commit()
+  conectar.close()
+
